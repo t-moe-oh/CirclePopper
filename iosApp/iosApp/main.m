c7c0f22104ad78d50b80525fe4fabb9eca19d84f
Developer <developer@circlepopper.game>
2026-05-10 12:36:54 +0200

    Fix IPA structure: ObjC entry point + manual .app bundle
    - Replace SwiftUI @main iOSApp.swift with ObjC main.m
    - Hardcode Info.plist values (no Xcode build variables)
    - Build IPA with clang + codesign ad-hoc + proper Payload/CirclePopper.app structure

---
+++ iosApp/iosApp/main.m
@@ -0,0 +1,21 @@
+#import <UIKit/UIKit.h>
+#import <shared/shared.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [CirclePopperKt MainViewController];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
