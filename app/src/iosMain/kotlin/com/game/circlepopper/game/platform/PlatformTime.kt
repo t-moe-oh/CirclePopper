4cf2dcb0a658641223ea6e5f7afb2d4cb5ff6d9d
Developer <developer@circlepopper.game>
2026-05-10 11:41:14 +0200

    Fix PlatformTime import: CFAbsoluteTimeGetCurrent is in CoreFoundation

--- app/src/iosMain/kotlin/com/game/circlepopper/game/platform/PlatformTime.kt
+++ app/src/iosMain/kotlin/com/game/circlepopper/game/platform/PlatformTime.kt
@@ -1,6 +1,6 @@
 package com.game.circlepopper.game.platform
 
-import platform.Foundation.CFAbsoluteTimeGetCurrent
+import platform.CoreFoundation.CFAbsoluteTimeGetCurrent
 
 actual fun currentTimeMillis(): Long =
     ((CFAbsoluteTimeGetCurrent() + 978307200.0) * 1000.0).toLong()
